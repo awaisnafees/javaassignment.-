3b6318f5763e4cd30587807966dac9300caa1940
awaisnafees <awaisnafees96@gmail.com>
2020-07-24 13:05:47 -0700

    java assignment completed

---
+++ app.js
@@ -0,0 +1,257 @@
+
+
+// var a="Pakistani";
+// console.log(a.charAt(7));
+
+// var a="Hello World";
+// console.log(a.charAt(9));
+
+// var a="Pakistani";
+// console.log(a.charAt(3));
+
+
+// var x= "Enter your name";
+// var y="your name is:"
+// var a=+prompt(x,y);
+// function greeting(awais){
+//         alert("0")
+//     }
+//     greeting("");
+
+
+
+
+// var city = "Hyderabad";
+// var city1 = city.replace("Hyderabad","Islamabad");
+// console.log(city1)
+
+
+
+// var a = ("ali and sami are best friends. they play cricket and football together");
+// document.write(a)
+// a.toString()
+
+
+// var foot=("472");
+// document.write(foot)
+
+// var name = prompt();
+// name = name.toUpperCase();
+// document.write(name)
+
+
+// var Bakery=prompt("")
+// var arr = ["cake","pastry"]
+// for (var i=0;i<arr.length;i++){
+//     if (arr[i] === Bakery){
+//         alert("cake is available")
+//     }
+    
+
+//     else{
+//         alert("We are Soory. pastry is not available in our baqkery")
+//     }
+    
+// }
+
+// var name= +prompt("Enter your password");
+// if (name === "1","2","3"){
+//     alert("password can not begin with num Please enter a valid password")
+// }
+
+// var word = ("university of karachi");
+// var check;
+
+// for ( var i= word.length -1; i >=0; i--){
+// console.log(word[i])
+// }
+
+
+
+// var a="Pakistan";
+// console.log(a.charAt(7));
+
+
+
+
+// var  num = 3.45214;
+// var round = Math.round(num);
+// var round = Math.floor(num);
+// var round = Math.ceil(num);   
+// console.log(round)
+
+// var num = -2.673
+// var round = Math.round(num);
+// var round = Math.floor(num);
+// var round = Math.ceil(num);
+// console.log(round)
+
+
+// var a="-4 is 4";
+// document.write("the absolute value of"+a)
+
+// var value="4";
+// var value1="6";
+// alert("random dice value:"+value)
+// document.write("random dice value:"+value1)
+
+
+// var headuser =("2");
+// var tailuser =("1");
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+// if(floor === 0){
+//     alert("" + headuser + " random coin value : Head")
+// }else {
+//     alert(""  + tailuser + " random coin value : Tail ")
+// }
+
+
+// var man=+prompt("Enter your Weight");
+// var bod="kg";
+// var body=man+bod;
+// document.write("the weight of user is"+body)
+
+
+
+// var x=prompt("Enter a number between 1 and 10")
+// for(x;x<10;x++){
+//     document.write(x)
+// }
+
+
+
+// task 31 to 34
+
+
+
+// var a = new Date();
+// console.log(a)
+
+
+
+
+// var month = new Array();
+//   month[0] = "January";
+//   month[1] = "February";
+//   month[2] = "March";
+//   month[3] = "April";
+//   month[4] = "May";
+//   month[5] = "June";
+//   month[6] = "July";
+//   month[7] = "August";
+//   month[8] = "September";
+//   month[9] = "October";
+//   month[10] = "November";
+//   month[11] = "December";
+
+//   var d = new Date();
+//   var n = month[d.getMonth()];
+//   alert("Current Month:"+n)
+
+
+
+// var d = new Date();
+//   var weekday = new Array(7);
+//   weekday[0] = "Sunday";
+//   weekday[1] = "Monday";
+//   weekday[2] = "Tuesday";
+//   weekday[3] = "Wednesday";
+//   weekday[4] = "Thursday";
+//   weekday[5] = "Friday";
+//   weekday[6] = "Saturday";
+
+//   var n = weekday[d.getDay()];
+//   alert("Today is" +n)
+
+
+
+// var a= new Date();
+// document.write("Current Date:"+a)
+
+// var d = new Date(2015, 11, 05, 23, 09, 37, 0);
+// document.write(d)
+
+
+// var d = new Date();
+//   var weekday = new Array(7);
+//   weekday[0] = "Sunday";
+//   weekday[1] = "Saturday";
+//   var n = weekday[d.getDay()];
+//   alert("its Fun Day"+n)
+
+
+// var dob = new Date (prompt("enter ur date of birth"))
+// var dobmili = dob.getTime();
+// var today = new Date();
+// var todaymili = today.getTime();
+// var diff = todaymili - dobmili;
+// var accuage = Math.floor(diff/(1000*60*60*24*30*12))
+// document.write(accuage);
+
+
+
+
+// var a=2;
+// var b=2;
+// var c=--a ;
+// alert(c)
+
+// var a=2;
+// var b=2;
+// var c=--a - --b ;
+// alert(c)
+
+// var a=2;
+// var b=2;
+// var c=--a - --b + ++b ;
+// alert(c)
+
+
+// var a=2;
+// var b=2;
+// var c=--a - --b + ++b + b--;
+// alert(c)
+
+
+// var name=prompt("Enter your name","your name is");
+// alert(name)
+
+
+
+
+
+// var name = "AWAIS";
+// name = name.toLowerCase();
+// console.log(name)
+
+// var name = "awais";
+// name = name.toUpperCase();
+// console.log(name)
+
+
+// var number=prompt("enter  number");
+// if(number >(10)){
+//     alert("larger")
+// }
+// else(number < = 10){
+//     alert("equal")
+// }
+
+
+// var a ="5";
+// if (a==5){
+//     alert ("true")
+// }
+// else{
+//     alert("false")
+// }
+
+
+// var names =["Awais"];
+// if(var i = 0; i < names.length; i++){
+//     alert("true")
+// }
+
+
+
